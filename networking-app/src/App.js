9c4a4963ec2e802161fce250b20e327029f4c35e
Ana Melissa <ana.melissa.connect@gmail.com>
2024-11-04 16:42:39 -0800

    adding workint app.js

--- networking-app/src/App.js
+++ networking-app/src/App.js
@@ -2,17 +2,17 @@
 // it includes the routing logic to switch between the home page and differnet chapters
 
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Chapter from './components/Chapter';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/chapter/:id" component={Chapter} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chapter/:id" element={<Chapter />} />
+      </Routes>
     </Router>
   );
 };
